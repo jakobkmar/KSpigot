974763c92ee5e628d411b4b55eb4d8b003121926
bluefireoly <bluefireolymp@gmail.com>
2020-07-06 00:55:47 +0200

    Created UnsafeImplementation annotation

---
+++ src/main/kotlin/net/axay/kspigot/annotations/KSpigotAnnotations.kt
@@ -0,0 +1,9 @@
+package net.axay.kspigot.annotations
+
+/**
+ * This element is useful, but not
+ * safe to use.
+ * Its implementation does not promise
+ * to always give the expected results.
+ */
+annotation class UnsafeImplementation
