ab5ca22d3a4181809280d073b4ddb0e5e7dc7809
Jakob K <bluefireolymp@gmail.com>
2020-07-23 20:11:04 +0200

    Added alternative for deprecated isOnGround

--- src/main/kotlin/net/axay/kspigot/extensions/entities/EntityExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/entities/EntityExtensions.kt
@@ -4,3 +4,5 @@ import org.bukkit.Material
 import org.bukkit.entity.Entity
 
 val Entity.isInWater: Boolean get() = this.location.block.type == Material.WATER
+
+val Entity.isGroundSolid: Boolean get() = this.location.add(0.0, -0.01, 0.0).block.type.isSolid
