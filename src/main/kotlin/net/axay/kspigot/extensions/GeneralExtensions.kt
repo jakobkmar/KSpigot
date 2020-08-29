1d92a53a2bdf367c22e76df49f95344ae00465d7
bluefireoly <bluefireolymp@gmail.com>
2020-08-29 19:18:53 +0200

    Added documentation to GeneralExtensions

--- src/main/kotlin/net/axay/kspigot/extensions/GeneralExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/GeneralExtensions.kt
@@ -4,16 +4,19 @@ import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 /**
+ * Shortcut to get all online players.
  * @see Bukkit.getOnlinePlayers
  */
 val onlinePlayers: Collection<Player> get() = Bukkit.getOnlinePlayers()
 
 /**
+ * Shortcut to get the Server.
  * @see Bukkit.getServer
  */
 val server by lazy { Bukkit.getServer() }
 
 /**
+ * Shortcut to get the PluginManager.
  * @see Bukkit.getPluginManager
  */
 val pluginManager by lazy { Bukkit.getPluginManager() }
