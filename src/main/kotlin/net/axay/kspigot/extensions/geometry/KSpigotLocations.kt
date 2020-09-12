57b2d75d3bd000db9c8aa64d7da40d10939a28fe
bluefireoly <bluefireolymp@gmail.com>
2020-09-12 13:22:05 +0200

    Chunk support

--- src/main/kotlin/net/axay/kspigot/extensions/geometry/KSpigotLocations.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/geometry/KSpigotLocations.kt
@@ -9,10 +9,14 @@ data class SimpleLocation2D(val x: Double, val y: Double) {
 
 data class SimpleLocation3D(val x: Double, val y: Double, val z: Double) {
     constructor(x: Number, y: Number, z: Number) : this(x.toDouble(), y.toDouble(), z.toDouble())
+    val chunk: SimpleChunkLocation get() = SimpleChunkLocation(x.toInt() shr 4, z.toInt() shr 4)
 }
+
+data class SimpleChunkLocation(val x: Int, val z: Int)
 
 // EXTENSIONS
 
 fun SimpleLocation3D.withWorld(world: World) = Location(world, x, y, z)
+fun SimpleChunkLocation.withWorld(world: World) = world.getChunkAt(x, z)
 
 val Location.worldOrException: World get() = world ?: throw NullPointerException("The world of the location is null!")
