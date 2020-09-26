f856b8d227f4d7162a04c2897cffc032abc736e7
bluefireoly <bluefireolymp@gmail.com>
2020-09-27 00:43:35 +0200

    Update GeneralExtensions.kt

--- src/main/kotlin/net/axay/kspigot/kotlinextensions/GeneralExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/kotlinextensions/GeneralExtensions.kt
@@ -1,5 +1,6 @@
 package net.axay.kspigot.kotlinextensions
 
 inline fun <T, R> Lazy<T>.ifInitialized(block: (T) -> R) = if (isInitialized()) block(value) else null
+val <T> Lazy<T>.valueIfInitialized get() = ifInitialized { value }
 
 fun Lazy<AutoCloseable>.closeIfInitialized() = ifInitialized { value.close() }
