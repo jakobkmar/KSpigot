24fa45dccd7fb3513212cfdd4d54ba4c3fb52b95
bluefireoly <bluefireolymp@gmail.com>
2020-09-29 20:46:22 +0200

    Added MinMaxPair

--- src/main/kotlin/net/axay/kspigot/kotlinextensions/GeneralExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/kotlinextensions/GeneralExtensions.kt
@@ -4,3 +4,20 @@ inline fun <T, R> Lazy<T>.ifInitialized(block: (T) -> R) = if (isInitialized()) 
 val <T> Lazy<T>.valueIfInitialized get() = ifInitialized { value }
 
 fun Lazy<AutoCloseable>.closeIfInitialized() = ifInitialized { value.close() }
+
+class MinMaxPair<T : Comparable<T>>(a: T, b: T) {
+
+    val min: T
+    val max: T
+
+    init {
+        if (a >= b) {
+            min = b
+            max = a
+        } else {
+            min = a
+            max = b
+        }
+    }
+
+}
