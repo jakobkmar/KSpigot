e43911f426a2dfad724103a5eb63a20e25f74e15
bluefireoly <bluefireolymp@gmail.com>
2020-10-15 19:03:54 +0200

    Create PluginFile.kt

---
+++ src/main/kotlin/net/axay/kspigot/config/PluginFile.kt
@@ -0,0 +1,12 @@
+package net.axay.kspigot.config
+
+import net.axay.kspigot.main.KSpigotMainInstance
+import java.io.File
+
+class PluginFile(path: String, child: String? = null)
+    : File(
+        "${KSpigotMainInstance.dataFolder}",
+        run {
+            if (child == null) path else File(path, child).path
+        }
+    )
