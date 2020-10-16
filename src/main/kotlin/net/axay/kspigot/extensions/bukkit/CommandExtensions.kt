c20604a07703f396cd6d9d0a64af9f6990d13ff8
bluefireoly <bluefireolymp@gmail.com>
2020-10-17 00:43:04 +0200

    Changed command registration

---
+++ src/main/kotlin/net/axay/kspigot/extensions/bukkit/CommandExtensions.kt
@@ -0,0 +1,19 @@
+package net.axay.kspigot.extensions.bukkit
+
+import net.axay.kspigot.main.KSpigotMainInstance
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.TabCompleter
+
+/**
+ * Registers this CommandExecutor for
+ * the given command.
+ */
+fun CommandExecutor.register(commandName: String): Boolean {
+    KSpigotMainInstance.getCommand(commandName)?.let {
+        it.setExecutor(this)
+        if (this is TabCompleter)
+            it.tabCompleter = this
+        return true
+    }
+    return false
+}
