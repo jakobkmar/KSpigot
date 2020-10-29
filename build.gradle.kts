a263099b39efa80d78a3e89b2ccf392b19cd9363
bluefireoly <bluefireolymp@gmail.com>
2020-10-29 16:51:16 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val JVM_VERSION_STRING = JVM_VERSION.versionString
  */
 
 group = "net.axay"
-version = "v1.16.3_R16"
+version = "v1.16.3_R17"
 
 description = "A Kotlin API for the Minecraft Server Software \"Spigot\"."
 
