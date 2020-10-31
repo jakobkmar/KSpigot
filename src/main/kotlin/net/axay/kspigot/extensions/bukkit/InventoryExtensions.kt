28505ec95bc4b34e5291b3c527865c05dc051981
bluefireoly <bluefireolymp@gmail.com>
2020-10-31 15:11:46 +0100

    Update InventoryExtensions.kt

--- src/main/kotlin/net/axay/kspigot/extensions/bukkit/InventoryExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/bukkit/InventoryExtensions.kt
@@ -1,5 +1,14 @@
 package net.axay.kspigot.extensions.bukkit
 
+import org.bukkit.event.inventory.InventoryAction
 import org.bukkit.inventory.Inventory
 
 fun Inventory.closeForViewers() = viewers.forEach { it.closeInventory() }
+
+val InventoryAction.isSimple
+    get() = when (this) {
+        InventoryAction.PLACE_ALL, InventoryAction.PLACE_ONE,
+        InventoryAction.PICKUP_ALL, InventoryAction.PICKUP_HALF, InventoryAction.PICKUP_ONE
+        -> true
+        else -> false
+    }
