1fb7d24e3af420cb0248fe5c7f78caca98051d3d
bluefireoly <bluefireolymp@gmail.com>
2021-01-10 22:24:30 +0100

    Switched development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val JVM_VERSION_STRING = JVM_VERSION.versionString
  */
 
 group = "net.axay"
-version = "v1.16.4_R21"
+version = "v1.16.4_R22"
 
 description = "A Kotlin API for the Minecraft Server Software \"Spigot\"."
 
