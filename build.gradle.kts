b8c54c910b5cee8e743d24255d004985c8ea8290
Jakob K <bluefireolymp@gmail.com>
2021-03-20 14:58:10 +0100

    Use gradle PasswordCredentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,10 +64,7 @@ tasks.dokkaHtml.configure {
 publishing {
     repositories {
         maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
-            credentials {
-                username = property("ossrh.username") as String
-                password = property("ossrh.password") as String
-            }
+            credentials(PasswordCredentials::class)
         }
     }
 
