490f9823edcf71f76f2d3a4c38096c47279e06a4
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-12 13:54:24 +0200

    Increase development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val githubRepo = "bluefireoly/KSpigot"
 
 group = "net.axay"
-version = "1.16.26"
+version = "1.16.27"
 
 description = "A Kotlin API for the Minecraft Server Software \"Spigot\"."
 
