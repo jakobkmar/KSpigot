addc4907889a45680f121295dc5fce0fbf9ad653
Jakob K <bluefireolymp@gmail.com>
2021-05-15 22:48:39 +0200

    Update LazyExtensions.kt

--- src/main/kotlin/net/axay/kspigot/languageextensions/kotlinextensions/LazyExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/languageextensions/kotlinextensions/LazyExtensions.kt
@@ -1,7 +1,7 @@
 package net.axay.kspigot.languageextensions.kotlinextensions
 
-internal inline fun <T, R> Lazy<T>.ifInitialized(block: (T) -> R) = if (isInitialized()) block(value) else null
+inline fun <T, R> Lazy<T>.ifInitialized(block: (T) -> R) = if (isInitialized()) block(value) else null
 
-internal val <T> Lazy<T>.valueIfInitialized get() = ifInitialized { value }
+val <T> Lazy<T>.valueIfInitialized get() = ifInitialized { value }
 
-internal fun Lazy<AutoCloseable>.closeIfInitialized() = ifInitialized { value.close() }
+fun Lazy<AutoCloseable>.closeIfInitialized() = ifInitialized { value.close() }
