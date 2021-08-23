4d3e25af32278fb991fd0da2f398a7cc0bbdfa32
Jakob K <bluefireolymp@gmail.com>
2021-08-24 01:36:16 +0200

    Make reflectField internal

--- src/main/kotlin/net/axay/kspigot/utils/ReflectionExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/utils/ReflectionExtensions.kt
@@ -3,7 +3,7 @@ package net.axay.kspigot.utils
 /**
  * Loads the value of the given field for this object.
  */
-fun <T> Any.reflectField(field: String): T {
+internal fun <T> Any.reflectField(field: String): T {
     val reflectedField = this::class.java.getDeclaredField(field)
     reflectedField.isAccessible = true
     @Suppress("UNCHECKED_CAST")
